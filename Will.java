1d0c6e0d0fb74eb7587a4a7490eff5dd61d13fde
Will Yi <yiwill07@gmail.com>
2023-09-25 22:02:28 -0400

    Added Missing Semicolon

--- Will.java
+++ Will.java
@@ -1,5 +1,12 @@
 public class Will {
   public static void main (String args[]) {
-    System.out.print("Will Yi")
+    System.out.print("Will Yi");
+    int m = 0;
+    while (true) {
+      System.out.print(m);
+      m++;
+      if (m > 10) break;
     }
+
+  }
 }
